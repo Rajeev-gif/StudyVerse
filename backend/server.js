15c32384bb02ab742acb2e0091237f50f09cbe0a
Rajeev.v <rajeevvalechha@gmail.com>
2026-01-03 13:13:09 +0530

    fixed socket cors issue - 3rd update

--- backend/server.js
+++ backend/server.js
@@ -52,8 +52,8 @@ app.use(
         ? "https://study-verse-opal.vercel.app"
         : "http://localhost:5173",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
 
--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const socket = io("https://studyverse-a1fp.onrender.com", {
   withCredentials: true,
-  autoConnect: false,
+  transports: ["websocket", "polling"],
 });
 
 export default socket;
